ea89c0236d60bf9fc8f721f7f9cd858046928285
mysecondspace <ihor.hontarenko@gmail.com>
2024-04-21 18:59:11 +0300

    Create a basic structure

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import logo from './images/logo.svg'
+
+import styles from './App.module.scss'
+
+function App() {
+  return (
+    <div className={styles.app}>
+      <img src={logo} alt='logo' />
+    </div>
+  )
+}
+
+export default App
